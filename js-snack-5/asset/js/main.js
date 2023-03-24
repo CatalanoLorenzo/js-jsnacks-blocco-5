c6456af07367f862c8871b8b1ddfbe12308aac2b
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-24 12:57:47 +0100

    add comments

--- js-snack-5/asset/js/main.js
+++ js-snack-5/asset/js/main.js
@@ -1,12 +1,12 @@
 /* Scrivi una funzione che accetti tre argomenti:
 un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b" */
-const array = ['a','b','c','d','e']
+const array = ['a', 'b', 'c', 'd', 'e']
 
 let a = 2
 let b = 4
 
-const new_array = newArray_whit_elementpositio_a_and_b(array,a,b)
+const new_array = newArray_whit_elementpositio_a_and_b(array, a, b)
 
 /**
  * 
@@ -14,12 +14,21 @@ const new_array = newArray_whit_elementpositio_a_and_b(array,a,b)
  * @param {Number} a 
  * @param {Number} b 
  */
-function newArray_whit_elementpositio_a_and_b(array,a,b) {
-    if (a<b && b<=array.length)
-    {const valoreA = array[a]
-    const valoreB = array[b]
-    const newArray = [valoreA,valoreB]
-    console.log(newArray);
-    return newArray}
-    else
+function newArray_whit_elementpositio_a_and_b(array, a, b) {
+    // se verifica la consizione a piu piccolo di b e b piu piccolo della lunghezza dell'array
+    if (a < b && b <= array.length) {
+        //associa ad una variabile il valore dell'array con index a
+        const valoreA = array[a]
+        //associa ad una variabile il valore dell'array con index b
+        const valoreB = array[b]
+        //crea un nuovo array con i valori A & B
+        const newArray = [valoreA, valoreB]
+        console.log(newArray);
+        //ritorna l'array
+        return newArray
+    }
+    else {
+        //se verifica la consizione a piu piccolo di b e b piu piccolo della lunghezza dell'array se è negativa
+        return console.log('error !a<b && b<=array.length');
+    }
 }
