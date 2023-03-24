32ad554ed810fc6771a9ca1ee3e849b4fdf74735
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-24 12:15:56 +0100

    initial snack 1

---
+++ js-snack-1/asset/js/main.js
@@ -0,0 +1,57 @@
+/* Snack 1:
+Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine. */
+//array di 10 oggetti
+const zucchine = [
+    {
+        varieta:'BlackBeauty',
+        peso:100,
+        lunfhezza:10
+    },
+    {
+        varieta:'CostataRomanesca',
+        peso:150,
+        lunfhezza:15
+    },
+    {
+        varieta:'TrombettaAlbenga',
+        peso:200,
+        lunfhezza:20,
+    },
+    {
+        varieta:'BiancaTrieste',
+        peso:250,
+        lunfhezza:25,
+    },
+    {
+        varieta:'TondaNizza',
+        peso:150,
+        lunfhezza:12
+    },
+    {
+        varieta:'LungaFiorentina',
+        peso:300,
+        lunfhezza:30
+    },
+    {
+        varieta:'StriataItalia',
+        peso:200,
+        lunfhezza:18
+    },
+    {
+        varieta:'PatissonBianco',
+        peso:150,
+        lunfhezza:14
+    },
+    {
+        varieta:'YellowCrookneck',
+        peso:250,
+        lunfhezza:22
+    },
+    {
+        varieta:'Cocozelle',
+        peso:250,
+        lunfhezza:22
+    },
+]
+const  { ,peso} = zucchine
+console.log(peso);
